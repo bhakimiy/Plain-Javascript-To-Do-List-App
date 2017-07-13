1a4001649691bac5c5ca51f5e3dae3d7921c23d6
Bobur Khakimov <mrbob0912@gmail.com>
2017-07-13 16:59:57 -0500

    Added some dragging functionality
    The dragging functionality is ready for 30-35 percent. Right now, thinking about how implement drag and drop as a child element.

--- assets/script.js
+++ assets/script.js
@@ -69,15 +69,19 @@ var AppController = (function (ListController, UIController) {
             }
         };
 
-        // Create a function that adds element to the List on the page and to the ListController
+        // Create a function that adds an element to the List on the page and to the ListController
         function addItem () {
             var itemName = listItemInputElement.value;
             if(itemName){
                 var newListItem = ListController.addListItem(itemName);
                 var newListItemElement = UIController.addListItemToUI(newListItem);
-                toggleDraggable(newListItemElement);
-                // Adding OnChange listener to the new Element
-                checkboxOnChange.call(document.getElementById('checkbox-' + newListItem.id));
+
+                /* All the Dragging related operations made here */
+                toggleDraggable(newListItemElement);    // Making an Element draggable only when drag dots hover
+                DragController.attachDragEventListeners(newListItemElement);    // Attaching all the needed drag event listeners to new created Element
+                /* End Dragging related operations */
+
+                checkboxOnChange.call(document.getElementById('checkbox-' + newListItem.id));   // Adding OnChange listener to the new Element
 
                 if(listItemInputElement.classList.contains('not-filled')){
                     listItemInputElement.classList.remove('not-filled');
@@ -102,8 +106,81 @@ var AppController = (function (ListController, UIController) {
                 })
             }
         };
+        
+        var DragController = (function () {
+            var cur = null, isTmpCreated = false, tmpLi = null;     // Saving current moving element to edit the classes after drag finishes, and creating helper variables
 
-        // A function that makes the parent node of the
+            var handleDragStart = function (e) {
+                this.classList.add('moving');
+                e.dataTransfer.effectAllowed = 'move';
+                e.dataTransfer.setData('text/html', this.innerHTML);
+                cur = this;
+            };
+
+            var handleDragOver = function (e) {
+                e.preventDefault();
+                e.dataTransfer.dropEffect = 'move';
+
+                if(cur !== this){
+                    var thisRect = this.getBoundingClientRect();    // getBoundingClientRect() gives api where we can get the X, Y position and the height of the element on viewport
+                    var mouseYOnObject = thisRect.top + thisRect.height - e.pageY;
+                    var currentMousePositionRelativeToElement, mousePositionAtFirstPlaceholderCreated;
+
+
+
+
+                    function createPlaceholder() {
+                        tmpLi = document.createElement('li');       // Creating plain Li placeholder element
+                        tmpLi.classList.add('tmp-li');              // Adding the prepared css class
+                        tmpLi.style.height = thisRect.height;       // Setting the height of the element equal to other li elements' height.
+                        isTmpCreated = true;
+
+                        if(!isTmpCreated){                          // Check if li placeholder is created. And li placeholder create only if it was not created earlier
+                            if((thisRect.height / 2) < mouseYOnObject){     // If user drags the object on top half of the element, the placeholder is created before that element
+                                this.parentNode.insertBefore(tmpLi, this);
+                                mousePositionAtFirstPlaceholderCreated = 'top';
+                            } else {
+                                this.parentNode.insertBefore(tmpLi, this.nextSibling);  // Otherwise, it will be created after the element
+                                mousePositionAtFirstPlaceholderCreated = 'bottom';
+                            }
+                        }
+                    }
+                }
+
+
+                return false;
+            };
+
+            var handleDragEnter = function (e) {
+                if(isTmpCreated){
+
+                }
+            };
+
+            var handleDragLeave = function (e) {
+
+            };
+
+            var handleDragEnd = function (e) {
+                cur.classList.remove('moving');
+                if(tmpLi){
+                    tmpLi.remove();     // Removing the li placeholder
+                    tmpLi = null;
+                    isTmpCreated = false;   // Telling the drag controller that there is no any temporary li placeholder left, and it can be created if needed
+                }
+            };
+
+            return {
+                attachDragEventListeners: function (item) {
+                    item.addEventListener('dragstart', handleDragStart);
+                    item.addEventListener('dragover', handleDragOver);
+                    item.addEventListener('dragleave', handleDragLeave);
+                    item.addEventListener('dragend', handleDragEnd);
+                }
+            };
+        })();
+        
+        // A function that makes the parent node of the element draggable
         var toggleDraggable = function(el){
             el.firstChild.onmouseover = function(){
                 if(this){
@@ -116,6 +193,8 @@ var AppController = (function (ListController, UIController) {
                 }
             }
         };
+
+
     };
 
     return {
